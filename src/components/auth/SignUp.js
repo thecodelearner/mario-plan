cf39c5d7526828396eb2a3b4c9d61004f6df8631
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-17 18:28:29 +0530

    fix sign up button in signup component

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -47,7 +47,7 @@ class SignUp extends Component {
 					</div>
 
 					<div className="input-field">
-						<button className="btn pink lighten-1 z-depth-0">Log In</button>
+						<button className="btn pink lighten-1 z-depth-0">Sign Up</button>
 					</div>
 
 				</form>
