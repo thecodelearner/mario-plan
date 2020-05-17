5ef710b66646931498624538c5f04379afb53172
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-17 17:09:43 +0530

    add layout components and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <h1>mario plan!</h1>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
