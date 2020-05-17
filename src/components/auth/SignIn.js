7ed92a3c5c1b67cf77e2d23b13359addfbbef338
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-17 19:06:04 +0530

    add css stylings to index.css

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -35,7 +35,7 @@ class SignIn extends Component {
 					</div>
 
 					<div className="input-field">
-						<button className="btn pink lighten-1 z-depth-0">Log In</button>
+						<button className="btn green lighten-1 z-depth-0">Log In</button>
 					</div>
 
 				</form>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -47,7 +47,7 @@ class SignUp extends Component {
 					</div>
 
 					<div className="input-field">
-						<button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+						<button className="btn green lighten-1 z-depth-0">Sign Up</button>
 					</div>
 
 				</form>
