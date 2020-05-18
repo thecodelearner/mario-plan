94a7705f820fa6096d6aeaa91a220bd7e73888b3
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-18 23:05:12 +0530

    fetch project details from firebase

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,27 +1,50 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { firestore } from 'firebase'
 
 const ProjectDetails = (props) => {
-
-	const id = props.match.params.id;
-
-	return (
-		<div>
+	// const id = props.match.params.id;
+	const { project } = props;
+	if (project) {
+		return (
 			<div className="container section project-details">
 				<div className="card z-depth-0">
 					<div className="card-content">
-						<span className="card-title">Project Title - {id} </span>
-						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur reiciendis ullam dolores autem minima eligendi ad? Nesciunt, provident! Tenetur, adipisci deleniti officiis quisquam sed alias voluptatibus voluptatem commodi fugiat totam?</p>
+						<span className="card-title">{project.title} </span>
+						<p>{project.content}</p>
 
 						<br />	<br />
 						<div className="card-action grey lighten-4 grey-text">
-							<div>Posted by Sahil</div>
+							<div>Posted by {project.authorFirstName} {project.authorLastName} </div>
 							<div>17th may 2020, 12pm</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	} else {
+		return (
+			<div className="container center">
+				<p>Loading project...</p>
+			</div>
+		)
+	}
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+	const id = ownProps.match.params.id;
+	const projects = state.firestore.data.projects;
+	const project = projects ? projects[id] : null;
+
+	return {
+		project: project
+	}
+}
+
+
+export default compose(
+	connect(mapStateToProps),
+	firestoreConnect([{ collection: 'projects' }])
+)(ProjectDetails)
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,8 +5,8 @@ const ProjectSummary = ({ project }) => {
 		<div className="card z-depth-0 project-summary">
 			<div className="card-content grey-text text-darken-3">
 				<span className="card-title">{project.title}</span>
-				<p>Posted by Sahil</p>
-				<p className="grey-text">17th May 2020, 12pm</p>
+				<p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+				<p className="grey-text">posted on</p>
 			</div>
 		</div>
 	)
