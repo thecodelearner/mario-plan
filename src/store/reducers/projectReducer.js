43c36161cd5d93742b01c4ec4b08278c35dfec3f
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-18 19:42:41 +0530

    set up thunk with redux store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -4,11 +4,14 @@ const initState = {
 		{ id: '2', title: 'collect all the stars', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio a, earum minima maiores unde porro architecto saepe, consequatur dignissimos quam suscipit quasi numquam iure. Distinctio veritatis totam voluptas optio!' },
 		{ id: '3', title: 'egg hunt with yoshi', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio a, earum minima maiores unde porro architecto saepe, consequatur dignissimos quam suscipit quasi numquam iure. Distinctio veritatis totam voluptas optio!' }
 	]
-
 }
 
 
 const projectReducer = (state = initState, action) => {
+	switch (action.type) {
+		case 'CREATE_PROJECT': console.log('created project', action.project); break;
+		default: console.log('default case');
+	}
 	return state
 }
 
