b108d082d249fa8b33a7dfe35213ad695a151427
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-19 00:35:46 +0530

    add route guarding link to auth

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createProject } from '../../store/actions/projectActions'
+import { Redirect } from 'react-router-dom'
 
 class CreateProject extends Component {
 
@@ -22,6 +23,13 @@ class CreateProject extends Component {
 	}
 
 	render() {
+
+		const { auth } = this.props;
+		// console.log(this.props);
+		if (!auth.uid) return <Redirect to='/signin' />
+
+
+
 		return (
 			<div className="container">
 				<form onSubmit={this.handleSubmit} className="white">
@@ -48,6 +56,13 @@ class CreateProject extends Component {
 }
 
 
+const mapStateToProps = (state) => {
+	return {
+		auth: state.firebase.auth
+	}
+}
+
+
 const mapDispatchToProps = (dispatch) => {
 	return {
 		createProject: (project) => dispatch(createProject(project))
@@ -55,4 +70,4 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-export default connect(null, mapDispatchToProps)(CreateProject)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject)
