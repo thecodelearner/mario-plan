f866e651d176e29f051e2386ae86f99dfd1e6558
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-18 19:20:29 +0530

    add dummy data with redux store

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,11 @@
-const initState = {}
+const initState = {
+	projects: [
+		{ id: '1', title: 'help me find peach', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio a, earum minima maiores unde porro architecto saepe, consequatur dignissimos quam suscipit quasi numquam iure. Distinctio veritatis totam voluptas optio!' },
+		{ id: '2', title: 'collect all the stars', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio a, earum minima maiores unde porro architecto saepe, consequatur dignissimos quam suscipit quasi numquam iure. Distinctio veritatis totam voluptas optio!' },
+		{ id: '3', title: 'egg hunt with yoshi', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, odio a, earum minima maiores unde porro architecto saepe, consequatur dignissimos quam suscipit quasi numquam iure. Distinctio veritatis totam voluptas optio!' }
+	]
+
+}
 
 
 const projectReducer = (state = initState, action) => {
