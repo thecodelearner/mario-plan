8a06b4b2045418273472f926fc49395b59c20890
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-18 22:41:51 +0530

    get data from firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -30,8 +30,9 @@ class Dashboard extends Component {
 
 
 const mapStateToProps = (state) => {
+	console.log(state);
 	return {
-		projects: state.project.projects
+		projects: state.firestore.ordered.projects
 	}
 }
 
