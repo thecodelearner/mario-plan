848229b1c21245a215a926e32d6d763648041730
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-19 00:43:47 +0530

    add route guards to signup/signin

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 
 class SignUp extends Component {
 
@@ -21,6 +24,9 @@ class SignUp extends Component {
 	}
 
 	render() {
+		const { auth } = this.props;
+		if (auth.uid) return <Redirect to='/' />
+
 		return (
 			<div className="container">
 				<form onSubmit={this.handleSubmit} className="white">
@@ -56,4 +62,12 @@ class SignUp extends Component {
 	}
 }
 
-export default SignUp
+
+
+const mapStateToProps = (state) => {
+	return {
+		auth: state.firebase.auth
+	}
+}
+
+export default connect(mapStateToProps)(SignUp)
