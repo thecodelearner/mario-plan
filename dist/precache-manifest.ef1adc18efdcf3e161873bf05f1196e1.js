a9d65fc86b2ce1e3fd1bd8f1142a6ff2594515fc
Sahil Sanghavi <git.codelearner@gmail.com>
2020-05-19 20:23:00 +0530

    yarn build to dist

---
+++ dist/precache-manifest.ef1adc18efdcf3e161873bf05f1196e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5ec4b4b5152ed269674f7f707aea8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d4ad417bbc95ba97d5",
+    "url": "/static/css/main.0af94a8f.chunk.css"
+  },
+  {
+    "revision": "9256b1f872e8ce324a26",
+    "url": "/static/js/2.576dc88e.chunk.js"
+  },
+  {
+    "revision": "710b3d9c872a3fd6b73e885f45708cb5",
+    "url": "/static/js/2.576dc88e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d4ad417bbc95ba97d5",
+    "url": "/static/js/main.8f11200a.chunk.js"
+  },
+  {
+    "revision": "5ae5cdff232dc0214069",
+    "url": "/static/js/runtime-main.a8803374.js"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7f210d3149b551a2189549bdc26a474.js"
+  "/precache-manifest.ef1adc18efdcf3e161873bf05f1196e1.js"
 );
 
 self.addEventListener('message', (event) => {
